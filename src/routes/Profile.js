bb0266304acfa6be90f37e25c976596ef95653fd
GO Lee <golee.dev@gmail.com>
2020-10-09 22:16:47 +0900

    3.0 Edit profile

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,16 +1,49 @@
-import React from 'react'
-import { authService } from 'fbase';
+import React, { useEffect, useState } from 'react';
+import { authService, dbService } from 'fbase';
 import { useHistory } from 'react-router-dom/cjs/react-router-dom';
 
-const Profile = () => {
+const Profile = ({ refreshUser, userObj }) => {
+    const [newDisplayName, setNewDisplayName] = useState(userObj.displayName);
     const history = useHistory();
-
     const onLogOutClick = () => {
         authService.signOut();
         history.push("/");
     };
+    const getMyNweets = async () => {
+        const nweets = await dbService.collection("nweets").where("creatorId", "==", userObj.uid).orderBy("createdAt").get();
+        console.log(nweets.docs.map((doc) => doc.data()));  
+    }
+    useEffect(() => {
+        getMyNweets();
+    }, [])
+
+    const onChange = (event) => {
+        const {
+            target: { value },
+        } = event;
+        setNewDisplayName(value);
+    }
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        if (userObj.displayName !== newDisplayName) {
+            await userObj.updateProfile({
+                displayName: newDisplayName,
+            })
+            refreshUser();
+        }
+    };
+
     return (
     <>
+        <form onSubmit={onSubmit}>
+            <input
+                onChange={onChange}
+                type="text"
+                placeholder="Display Name" 
+                value={newDisplayName}
+            />    
+            <input type="submit" value="Update Profile"/>
+        </form>      
         <button onClick={onLogOutClick}>Log Out</button>
     </>
     );

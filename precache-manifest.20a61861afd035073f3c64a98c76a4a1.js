cccf93561cc2ce085eb6793b83ad833b9c8c4b3a
GO Lee <golee.dev@gmail.com>
2020-10-10 22:52:47 +0900

    Updates

---
+++ precache-manifest.20a61861afd035073f3c64a98c76a4a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64a562a570663cc447442d2e8699dcfb",
+    "url": "/nwitter/index.html"
+  },
+  {
+    "revision": "6986e4de84425052d910",
+    "url": "/nwitter/static/css/main.2ff3d120.chunk.css"
+  },
+  {
+    "revision": "9bd8337b5e3f5d425bad",
+    "url": "/nwitter/static/js/2.ac99a988.chunk.js"
+  },
+  {
+    "revision": "8e844923efaee6906e00c8fde9e55716",
+    "url": "/nwitter/static/js/2.ac99a988.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6986e4de84425052d910",
+    "url": "/nwitter/static/js/main.6b4d6a7d.chunk.js"
+  },
+  {
+    "revision": "97fb235846154051176c",
+    "url": "/nwitter/static/js/runtime-main.222ae764.js"
+  }
+]);
